9efc2e7234654eed2349523a9e2410fdac740546
pc-linux <patryk.stachowski@op.pl>
2023-09-12 18:54:33 +0200

    initial commit

---
+++ src/proto/exchange/mod.rs
@@ -0,0 +1,56 @@
+// @generated
+// @@protoc_insertion_point(attribute:event_provider_api)
+pub mod event_provider_api {
+    include!("event_provider_api.rs");
+    // @@protoc_insertion_point(event_provider_api)
+}
+// @@protoc_insertion_point(attribute:injective_accounts_rpc)
+pub mod injective_accounts_rpc {
+    include!("injective_accounts_rpc.rs");
+    // @@protoc_insertion_point(injective_accounts_rpc)
+}
+// @@protoc_insertion_point(attribute:injective_auction_rpc)
+pub mod injective_auction_rpc {
+    include!("injective_auction_rpc.rs");
+    // @@protoc_insertion_point(injective_auction_rpc)
+}
+// @@protoc_insertion_point(attribute:injective_derivative_exchange_rpc)
+pub mod injective_derivative_exchange_rpc {
+    include!("injective_derivative_exchange_rpc.rs");
+    // @@protoc_insertion_point(injective_derivative_exchange_rpc)
+}
+// @@protoc_insertion_point(attribute:injective_exchange_rpc)
+pub mod injective_exchange_rpc {
+    include!("injective_exchange_rpc.rs");
+    // @@protoc_insertion_point(injective_exchange_rpc)
+}
+// @@protoc_insertion_point(attribute:injective_explorer_rpc)
+pub mod injective_explorer_rpc {
+    include!("injective_explorer_rpc.rs");
+    // @@protoc_insertion_point(injective_explorer_rpc)
+}
+// @@protoc_insertion_point(attribute:injective_insurance_rpc)
+pub mod injective_insurance_rpc {
+    include!("injective_insurance_rpc.rs");
+    // @@protoc_insertion_point(injective_insurance_rpc)
+}
+// @@protoc_insertion_point(attribute:injective_meta_rpc)
+pub mod injective_meta_rpc {
+    include!("injective_meta_rpc.rs");
+    // @@protoc_insertion_point(injective_meta_rpc)
+}
+// @@protoc_insertion_point(attribute:injective_oracle_rpc)
+pub mod injective_oracle_rpc {
+    include!("injective_oracle_rpc.rs");
+    // @@protoc_insertion_point(injective_oracle_rpc)
+}
+// @@protoc_insertion_point(attribute:injective_portfolio_rpc)
+pub mod injective_portfolio_rpc {
+    include!("injective_portfolio_rpc.rs");
+    // @@protoc_insertion_point(injective_portfolio_rpc)
+}
+// @@protoc_insertion_point(attribute:injective_spot_exchange_rpc)
+pub mod injective_spot_exchange_rpc {
+    include!("injective_spot_exchange_rpc.rs");
+    // @@protoc_insertion_point(injective_spot_exchange_rpc)
+}
